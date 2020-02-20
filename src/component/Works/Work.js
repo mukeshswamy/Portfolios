cb35c43d7d4c059e2a80c4e8647b626eae8a85a2
mukeshswamy <59552894+mukeshswamy@users.noreply.github.com>
2020-02-20 20:45:29 +0530

    section-added-new

--- src/component/Works/Work.js
+++ src/component/Works/Work.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./Work.css";
 import { Container, Row, Col } from "react-bootstrap";
+import work1 from "../../assets/images/1.jpg";
+import work2 from "../../assets/images/05.jpg";
+import work3 from "../../assets/images/07.jpg";
+import work4 from "../../assets/images/08.jpg";
 
 function Work() {
   return (
     <section className="work-bg">
       <Container>
-        <Row>
+        <Row className="pb-4">
           <Col md={12}>
             <div className="work">
               <p>Works</p>
@@ -19,6 +23,26 @@ function Work() {
                 qui officia deserunt mollit anim id est laborum. Sed ut
                 perspiciatis unde omnis iste natus
               </p>
+            </div>
+          </Col>
+          <Col md={3}>
+            <div>
+              <img src={work1} className="work-img" alt="work1"/>
+            </div>
+          </Col>
+          <Col md={3}>
+          <div>
+              <img src={work2} className="work-img" alt="work2"/>
+            </div>
+          </Col>
+          <Col md={3}>
+          <div>
+              <img src={work3} className="work-img" alt="work3"/>
+            </div>
+          </Col>
+          <Col md={3}>
+          <div>
+              <img src={work4} className="work-img" alt="work4"/>
             </div>
           </Col>
         </Row>
